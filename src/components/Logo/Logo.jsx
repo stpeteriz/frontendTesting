ff68bc2c02590afc5aab940b90c0752dba7e95cd
stpeteriz <st.peteriz17@gmail.com>
2023-02-03 00:35:58 +0100

    uploading files

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+
+const Logo = () =>{
+    return(
+      <div className="ma4 mt0">
+        <Tilt reset={false} className='Tilt br2 shadow-2' style={{height: '150px', width: '150px'}}>
+            <div>
+                
+            </div>
+        </Tilt>
+
+      </div>
+    )
+}
+
+export default Logo;
